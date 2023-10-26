44ac5d42e35318746efbf7cdd422f156860f1076
TechMiX <mytechmix@gmail.com>
2023-10-26 09:05:23 +0200

    chore: extract more view related logic from vicky html

--- scripts/view/vicky.js
+++ scripts/view/vicky.js
@@ -400,6 +400,140 @@ function handleContent(file) {
     }).catch(error => console.log(error));
 }
 
+function handleUploader() {
+
+    // feature detection for drag&drop upload
+    var isAdvancedUpload = function () {
+        var div = document.createElement('div');
+        return (('draggable' in div) || ('ondragstart' in div && 'ondrop' in div)) && 'FormData' in window && 'FileReader' in window;
+    }();
+
+    // applying the effect for every form
+    var forms = document.querySelectorAll('.box');
+    Array.prototype.forEach.call(forms, function (form) {
+        var input = form.querySelector('input[type="file"]'),
+            label = form.querySelector('label'),
+            errorMsg = form.querySelector('.box__error span'),
+            restart = form.querySelectorAll('.box__restart'),
+            droppedFiles = false,
+            showFiles = function (files) {
+                label.textContent = files.length > 1 ? ('').replace('{count}', files.length) : files[0].name;
+            },
+            triggerFormSubmit = function () {
+                var event = document.createEvent('HTMLEvents');
+                //event.initEvent( 'submit', true, false );
+                //form.dispatchEvent( event );
+            };
+
+        // letting the server side to know we are going to make an Ajax request
+        var ajaxFlag = document.createElement('input');
+        ajaxFlag.setAttribute('type', 'hidden');
+        ajaxFlag.setAttribute('name', 'ajax');
+        ajaxFlag.setAttribute('value', 1);
+        form.appendChild(ajaxFlag);
+
+        // automatically submit the form on file select
+        input.addEventListener('change', function (e) {
+            showFiles(e.target.files);
+
+            triggerFormSubmit();
+
+        });
+
+        // drag&drop files if the feature is available
+        if (isAdvancedUpload) {
+            form.classList.add('has-advanced-upload'); // letting the CSS part to know drag&drop is supported by the browser
+
+            ['drag', 'dragstart', 'dragend', 'dragover', 'dragenter', 'dragleave', 'drop'].forEach(function (event) {
+                form.addEventListener(event, function (e) {
+                    // preventing the unwanted behaviours
+                    e.preventDefault();
+                    e.stopPropagation();
+                });
+            });
+            ['dragover', 'dragenter'].forEach(function (event) {
+                form.addEventListener(event, function () {
+                    form.classList.add('is-dragover');
+                });
+            });
+            ['dragleave', 'dragend', 'drop'].forEach(function (event) {
+                form.addEventListener(event, function () {
+                    form.classList.remove('is-dragover');
+                });
+            });
+            form.addEventListener('drop', function (e) {
+                droppedFiles = e.dataTransfer.files; // the files that were dropped
+                showFiles(droppedFiles);
+                setTimeout(function () {
+                    form.classList.remove('is-uploading');
+                    form.classList.add('is-success');
+                }, 300);
+                handleContent(droppedFiles[0]);
+                //getFile( document.getElementById( "file" ) );
+                triggerFormSubmit();
+            });
+        }
+
+        // if the form was submitted
+        form.addEventListener('submit', function (e) {
+            // preventing the duplicate submissions if the current one is in progress
+            if (form.classList.contains('is-uploading')) return false;
+
+            form.classList.add('is-uploading');
+            form.classList.remove('is-error');
+
+            if (isAdvancedUpload) // ajax file upload for modern browsers
+            {
+                e.preventDefault();
+                setTimeout(function () {
+                    form.classList.remove('is-uploading');
+                    form.classList.add('is-success');
+                }, 300);
+            }
+            else // fallback Ajax solution upload for older browsers
+            {
+                var iframeName = 'uploadiframe' + new Date().getTime(),
+                    iframe = document.createElement('iframe');
+
+                $iframe = $('<iframe name="' + iframeName + '" style="display: none;"></iframe>');
+
+                iframe.setAttribute('name', iframeName);
+                iframe.style.display = 'none';
+
+                document.body.appendChild(iframe);
+                form.setAttribute('target', iframeName);
+
+                iframe.addEventListener('load', function () {
+                    //var data = JSON.parse( iframe.contentDocument.body.innerHTML );
+                    setTimeout(function () {
+                        form.classList.remove('is-uploading');
+                        form.classList.add('is-success');
+                    }, 300);
+                    //form.classList.remove( 'is-uploading' )
+                    //form.classList.add( data.success == true ? 'is-success' : 'is-error' )
+                    form.removeAttribute('target');
+                    //if( !data.success ) errorMsg.textContent = data.error;
+                    iframe.parentNode.removeChild(iframe);
+                });
+            }
+        });
+
+        // restart the form if has a state of error/success
+        Array.prototype.forEach.call(restart, function (entry) {
+            entry.addEventListener('click', function (e) {
+                e.preventDefault();
+                form.classList.remove('is-error', 'is-success');
+                input.click();
+            });
+        });
+
+        // Firefox focus bug fix for file input
+        input.addEventListener('focus', function () { input.classList.add('has-focus'); });
+        input.addEventListener('blur', function () { input.classList.remove('has-focus'); });
+
+    });
+}
+
 function modalVanish() {
     document.getElementById("black-bg").style.display = "none";
     document.getElementById("modal").style.display = "none";
