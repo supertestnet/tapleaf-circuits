f3d37cb16d176f59df3e5b3c46dd25ec328a18c8
TechMiX <mytechmix@gmail.com>
2023-10-31 11:49:00 +0100

    chore: move saveData out of vm to paul view controller

--- scripts/vm.js
+++ scripts/vm.js
@@ -128,18 +128,6 @@ var generateBitCommitments = async () => {
     });
 }
 
-function saveData(data, fileName) {
-    var a = document.createElement("a");
-    document.body.appendChild(a);
-    a.style = "display: none";
-    var blob = new Blob([data], { type: "octet/stream" }),
-        url = window.URL.createObjectURL(blob);
-    a.href = url;
-    a.download = fileName;
-    a.click();
-    window.URL.revokeObjectURL(url);
-}
-
 var generateBitCommitmentAddress = (proverPubkey, verifierPubkey) => {
     var leaf1 = [
         "OP_10",
