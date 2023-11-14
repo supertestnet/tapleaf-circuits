0c96af10247c842b7b5e2a59e132973c7ce68b9d
TechMiX <mytechmix@gmail.com>
2023-11-14 19:18:34 +0100

    feat: add 3-bit shifter program

---
+++ scripts/circuits/bristol_shifter.js
@@ -0,0 +1,12 @@
+var circuit_bristol_shifter = `
+6 10
+2 1 3
+1 3
+
+1 1 0 4 INV
+2 1 0 1 5 AND
+2 1 2 0 9 AND
+2 1 2 4 7 AND
+2 1 3 4 6 AND
+2 1 6 5 8 OR
+`;
--- scripts/view/paul.js
+++ scripts/view/paul.js
@@ -5,15 +5,24 @@ $('.view_caveats').onclick = () => {
 $('.view_zero_checker').onclick = () => {
     $('.zero_checker').classList.remove("hidden");
     $('.size_checker').classList.add("hidden");
+    $('.shifter').classList.add("hidden");
     $('.addition').classList.add("hidden");
 }
 $('.view_size_checker').onclick = () => {
     $('.zero_checker').classList.add("hidden");
     $('.size_checker').classList.remove("hidden");
+    $('.shifter').classList.add("hidden");
     $('.addition').classList.add("hidden");
 }
 $('.view_addition').onclick = () => {
     $('.zero_checker').classList.add("hidden");
     $('.size_checker').classList.add("hidden");
+    $('.shifter').classList.add("hidden");
     $('.addition').classList.remove("hidden");
+}
+$('.view_shifter').onclick = () => {
+    $('.zero_checker').classList.add("hidden");
+    $('.size_checker').classList.add("hidden");
+    $('.addition').classList.add("hidden");
+    $('.shifter').classList.remove("hidden");
 }
